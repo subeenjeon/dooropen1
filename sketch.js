da029aea6ce5e20bca553e32c32de2a24f431140
subeenjeon <powersubeen@naver.com>
2014-11-18 20:10:39 +0900

    fdf

--- sketch.js
+++ sketch.js
@@ -4,13 +4,16 @@ var count;
 
 function setup(){
 	createCanvas(420,600);
-	background(255);
 	count=0;
   	open=0;
   	openCount=0;
 }
 
 function draw() {
+
+	drawBG();
+
+
 	drawDoor(30, 370, 10, open);
 	open=open-1;
 	if(open>-10){
